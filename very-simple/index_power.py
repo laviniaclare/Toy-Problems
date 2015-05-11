670ebee27c856fc8b5bc3b99dd0d0fa46f77408d
Lavinia Karl <laviniaclare@gmail.com>
2015-05-11 15:45:18 -0700

    added new folder

--- very-simple/index_power.py
+++ very-simple/index_power.py
@@ -1,8 +1,9 @@
-'''Given an array with positive numbers and a number N find the N-th power of the element in the array with the index N. If N is outside of the array, then return -1.
+'''Given an array with positive numbers and a number "n" find the n-th power of the element in the array with the index n. 
+If n is outside of the array, then return -1.
 
 Examples:
-- array = [1, 2, 3, 4] and N = 2, then the result is 3 ^ 2 == 9;
-- array = [1, 2, 3] and N = 3, but N is outside of the array, so the result is -1.'''
+- array = [1, 2, 3, 4] and n = 2, then the result is 3 ^ 2 == 9;
+- array = [1, 2, 3] and n = 3, but N is outside of the array, so the result is -1.'''
 
 
 def index_power(array, n):
