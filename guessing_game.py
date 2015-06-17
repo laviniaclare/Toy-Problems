f69638871b64f1750f6b06b158f9bc7b33a7cae1
Lavinia Karl <laviniaclare@gmail.com>
2015-06-17 16:10:59 -0700

    Adding guessing_game

---
+++ guessing_game.py
@@ -0,0 +1,9 @@
+
+'''Create an interactive guessing game.  The program should ask for a users name,
+pick a random number between 1-100, give teh user hints if they guess wrong,
+and congratulate the user before exiting the program if a user guesses correctly'''
+
+
+def guessing_game():
+    user_name = raw_input("hello.  Welcome to my game.  What should I call you?")
+    print user_name
