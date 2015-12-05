ac3f5d5348db0ed9889b53bbbc025ab6f264b838
laviniaclare <laviniaclare@gmail.com>
2015-12-04 17:24:13 -0800

    added another solution

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,10 +1,12 @@
 '''Create a function called "fizz_buzz" which takes a number n and prints all the numbers from zero to n, replacing multiples of 3 with the string "Fizz", multiples of 5 with the string "Buzz", and multiples of both 5 and 3 with the string "FizzBuzz".
 
-Example: If n = 15 the funtion should print 0, 1, 2, Fizz, 4, Buzz, 6, 7, 8, 9, Buzz, 11, Fizz, 13, 14, FizzBuzz'''
+Example: If n = 15 the funtion should print 1, 2, Fizz, 4, Buzz, 6, 7, 8, 9, Buzz, 11, Fizz, 13, 14, FizzBuzz'''
 
 
 def fizz_buzz(n):
-    i = 0
+    '''Solution using a while-loop
+    '''
+    i = 1
     while i < n:
         output = ''
         if not i % 3:
@@ -16,4 +18,16 @@ def fizz_buzz(n):
         print output
         i += 1
 
-fizz_buzz(30)
+
+def fizz_buzz2(n):
+    '''Solution using a for-loop
+    '''
+    for num in range(1, n+1):
+        output = ""
+        if not num % 3:
+            output += "Fizz"
+        if not num % 5:
+            output += "Buzz"
+        if output == "":
+            output = num
+        print output
