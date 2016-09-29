5d9b72504bd43940c35f678a918d8d1a1cc869c3
laviniaclare <laviniaclare@gmail.com>
2016-09-29 13:20:00 -0700

    added test for even tail

--- linked-lists/ll_tests.py
+++ linked-lists/ll_tests.py
@@ -13,13 +13,15 @@ class RemoveOddsTests(unittest.TestCase):
         one_odd_list = LinkedList()
         one_odd_list.add_node(node)
         one_odd_list.remove_odds()
+
+        "it should produce and empty list has only one node, which is odd"
         self.assertTrue(one_odd_list.is_empty())
 
     def test_one_even_node(self):
         one_even_list = LinkedList()
         one_even_list.add_node(Node(2))
         one_even_list.remove_odds()
-        
+        "it should not change the list if the list has ony one node, which is even"
         self.assertEqual(one_even_list.get_length(), 1)
 
     def test_tail_odd(self):
@@ -29,11 +31,20 @@ class RemoveOddsTests(unittest.TestCase):
             odd_tail.add_node(Node(node_val))
 
         odd_tail.remove_odds()
+        "it should remove the tail if the tail is odd"
         self.assertEqual(odd_tail.tail.data, 4)
         self.assertEqual(odd_tail.get_length(), 2)
 
     def test_tail_even(self):
-        pass
+        even_tail = LinkedList()
+        node_vals = [2, 3, 4, 10]
+        for node_val in node_vals:
+            even_tail.add_node(Node(node_val))
+
+        even_tail.remove_odds()
+        "it should remove odds if the tail is even"
+        self.assertEqual(even_tail.tail.data, 10)
+        self.assertEqual(even_tail.get_length(), 3)
 
     def test_head_even(self):
         pass
